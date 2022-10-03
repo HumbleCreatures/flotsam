e01ec9305f116c5ace7939ca9a7ef9b20a9c88b0
Jonatan Hallenberg <jonatan.hallenberg@sprinto.se>
2022-10-03 14:22:10 +0200

    more tips

--- apps/flotsam-web/src/app/AssignmentTest.tsx
+++ apps/flotsam-web/src/app/AssignmentTest.tsx
@@ -3,14 +3,21 @@ import { getCategoryByName } from '@flotsam/flotsam-shared';
 
 const AssignmentTest = () => {
 
+type CategoryWithCount = {
+    name: string,
+    id: number,
+    productCount: number
+}
+
     useEffect(() => {
         const loadData = async () => {
             const products = await (await fetch("https://fakestoreapi.com/products")).json();
             const productsWithCategories = await Promise.all(products.map(async (product: any) => {
-                const categoryForProduct: any = await getCategoryByName(product.category);
+                const categoryForProduct = await getCategoryByName(product.category) as CategoryWithCount;
                 categoryForProduct.productCount = products.filter((product: any) => product.category === categoryForProduct.name).length
                 return { ...product, category: categoryForProduct }
             }));
+            console.log(productsWithCategories.sort((a, b) => a.category.name.localeCompare(b.category.name) || a.title.localeCompare(b.title)));
         }
         loadData();
     }, [])
